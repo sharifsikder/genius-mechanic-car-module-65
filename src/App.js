064fe77dfd7d8339f6837be6cab413c20513ab24
John Doe <johndoe@example.com>
2021-10-27 11:35:49 +0600

    added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from './Pages/Login/Login/Login';
 import Header from './Pages/Shared/Header/Header';
 import AuthProvider from './contexts/AuthProvider';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
+import AddService from './Pages/AddService/AddService';
+import ManageService from './Pages/ManageService/ManageService';
 
 
 function App() {
@@ -25,9 +27,15 @@ function App() {
             <Route path="/login">
               <Login></Login>
             </Route>
+            <Route path="/manageService">
+              <ManageService></ManageService>
+            </Route>
             <PrivateRoute path="/booking/:serviceId">
               <Booking></Booking>
             </PrivateRoute>
+            <Route path="/addService">
+              <AddService></AddService>
+            </Route>
             <Route path="*">
               <NotFound></NotFound>
             </Route>
--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Booking = () => {
     const { serviceId } = useParams();
+
+    const [service, setServices] = useState({})
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${serviceId}`)
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [])
     return (
         <div>
+            <h1>Details of : {service.Name}</h1>
             <h2>this is booking: {serviceId}</h2>
         </div>
     );
